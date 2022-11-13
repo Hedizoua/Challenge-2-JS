32828d6e312cc0f58c53b3daa835770c4792b8a0
Hedizoua <hedizouari55@yahoo.fr>
2022-11-13 14:07:17 +0100

    add Ex4

---
+++ .vscode/ex4.js
@@ -0,0 +1,15 @@
+var a = "1"
+var b = "2"
+var c = a + b
+
+alert('Type of a :' + typeof a +
+     '\nType of b: ' + typeof b +
+     '\n Type of c:' + typeof c);
+
+a = Number(prompt("enter a (entier) "));
+b = Number(prompt('enter b (entier) '));
+c = a + b
+
+alert('Type of a :' + typeof a +
+     '\nType of b: ' + typeof b +
+     '\n Type of c:' + typeof c);
